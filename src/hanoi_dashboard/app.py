81eda4ba9a02fd87fc9e59722e6983096ed79d88
Tom Beihofer <tom@sirius23.de>
2025-04-17 17:24:29 +0200

    von html auf dash_mantine_components gewechselt

--- src/hanoi_dashboard/app.py
+++ src/hanoi_dashboard/app.py
@@ -1,9 +1,12 @@
+import dash
+import dash_mantine_components as dmc
 from dash import Dash
 
 from src.hanoi_dashboard.callbacks import register_home_callbacks
 
-app = Dash(__name__, use_pages=True)
+app = Dash(__name__, use_pages=True, external_stylesheets=dmc.styles.ALL)
 app.title = "Hanoi Sensor Data Dashboard"
+app.layout = dmc.MantineProvider(dash.page_container)
 
 # Register callbacks
 register_home_callbacks(app)
--- src/hanoi_dashboard/pages/home.py
+++ src/hanoi_dashboard/pages/home.py
@@ -1,32 +1,44 @@
 import dash
+import dash_mantine_components as dmc
 from dash import dcc, html
 
 dash.register_page(__name__, path="/")
 
-layout = html.Div(
+layout = dmc.Box(
     children=[
-        html.H1(
-            children=f"OpenSenseMap Sensor Data (Box: {'6252afcfd7e732001bb6b9f7'})"
+        dmc.Box(
+            mt=15,
+            mx=15,
+            children=[
+                dmc.Center(
+                    dmc.Title(
+                        children=f"OpenSenseMap Sensor Data (Box: {'6252afcfd7e732001bb6b9f7'})",
+                        order=1,
+                    ),
+                ),
+                dmc.Button(
+                    "Fetch Latest Data & Update Graphs", id="fetch-button", n_clicks=0
+                ),
+                dmc.Text(
+                    id="output-status",
+                    children="Dashboard loaded. Click button to fetch initial data.",
+                ),
+                # Interval component for automatic refresh (optional)
+                # dcc.Interval(
+                #     id='interval-component',
+                #     interval=5*60*1000, # in milliseconds (e.g., 5 minutes)
+                #     n_intervals=0
+                # ),
+                # Store for holding graph data (JSON format)
+                dcc.Store(id="graph-data-store"),
+                # Container where graphs will be dynamically added
+                dmc.Box(
+                    id="graph-container",
+                    children=[html.P("Graphs will appear here after fetching data.")],
+                ),
+                dmc.Divider(variant="solid"),
+                html.Footer("Dashboard End"),
+            ],
         ),
-        html.Button("Fetch Latest Data & Update Graphs", id="fetch-button", n_clicks=0),
-        html.Div(
-            id="output-status",
-            children="Dashboard loaded. Click button to fetch initial data.",
-        ),
-        # Interval component for automatic refresh (optional)
-        # dcc.Interval(
-        #     id='interval-component',
-        #     interval=5*60*1000, # in milliseconds (e.g., 5 minutes)
-        #     n_intervals=0
-        # ),
-        # Store for holding graph data (JSON format)
-        dcc.Store(id="graph-data-store"),
-        # Container where graphs will be dynamically added
-        html.Div(
-            id="graph-container",
-            children=[html.P("Graphs will appear here after fetching data.")],
-        ),
-        html.Hr(),
-        html.Footer("Dashboard End"),
     ]
 )
