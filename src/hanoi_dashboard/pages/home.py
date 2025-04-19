39abaf947a41e97df127a03799dfd45fe03ef318
Tom Beihofer <tom@sirius23.de>
2025-04-19 16:45:16 +0200

    Home-Page

--- src/hanoi_dashboard/pages/home.py
+++ src/hanoi_dashboard/pages/home.py
@@ -1,39 +1,13 @@
 import dash
 import dash_mantine_components as dmc
 
-dash.register_page(__name__, path="/")
+dash.register_page(__name__, path="/", redirect_from=["/home"])
 
 layout = dmc.Box(
     children=[
         dmc.Box(
-            mt=15,
-            mx=15,
-            children=[
-                # dmc.Center(
-                #     dmc.Title(
-                #         children=f"OpenSenseMap Sensor Data (Box: {'6252afcfd7e732001bb6b9f7'})",
-                #         order=1,
-                #     ),
-                #     pb=25,
-                # ),
-                dmc.Text(
-                    id="output-status",
-                    children="Dashboard loaded. Click button to fetch initial data.",
-                ),
-                # Container where graphs will be dynamically added
-                dmc.Box(
-                    id="graph-container",
-                    children=[
-                        dmc.Grid(
-                            id="graph-grid",
-                            gutter="md",
-                            children=[
-                                dmc.Text("Graphs will appear here after fetching data.")
-                            ],
-                        ),
-                    ],
-                ),
-            ],
+            m=15,
+            children=["Home Page"],
         ),
     ],
 )
