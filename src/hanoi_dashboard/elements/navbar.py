2b7e5c55f2caaef9515dc08ed26673c190aa68d7
Tom Beihofer <tom@sirius23.de>
2025-04-19 17:23:17 +0200

    Navbar mit Icons

--- src/hanoi_dashboard/elements/navbar.py
+++ src/hanoi_dashboard/elements/navbar.py
@@ -1,13 +1,32 @@
 import dash_mantine_components as dmc
+from dash_iconify import DashIconify
+
+
+def get_icon(icon: str) -> DashIconify:
+    return DashIconify(icon=icon, height=20)
 
 
 def create_navbar():
     return dmc.AppShellNavbar(
         id="navbar",
         children=[
-            dmc.NavLink(label="Home", href="/"),
-            dmc.NavLink(label="Sensors", href="/sensors"),
-            dmc.NavLink(label="Forecast", href="/forecasts"),
+            dmc.NavLink(
+                label="Home",
+                href="/",
+                leftSection=get_icon("bi:house-door-fill"),
+                rightSection=get_icon("tabler-chevron-right"),
+            ),
+            dmc.NavLink(
+                label="Sensors",
+                href="/sensors",
+                leftSection=get_icon("bi:cpu-fill"),
+                rightSection=get_icon("tabler-chevron-right"),
+            ),
+            dmc.NavLink(
+                label="Forecast",
+                href="/forecasts",
+                leftSection=get_icon("bi:graph-up-arrow"),
+                rightSection=get_icon("tabler-chevron-right"),
+            ),
         ],
-        p="md",
     )
