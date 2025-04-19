b6cf3af61a1f73e5315740e1dfee09df7ebd14da
Tom Beihofer <tom@sirius23.de>
2025-04-19 18:06:39 +0200

    Auswahlfelder für Sensoren

--- src/hanoi_dashboard/pages/sensors.py
+++ src/hanoi_dashboard/pages/sensors.py
@@ -1,3 +1,5 @@
+from datetime import date
+
 import dash
 import dash_mantine_components as dmc
 
@@ -8,6 +10,43 @@ layout = dmc.Box(
         dmc.Box(
             m=15,
             children=[
+                dmc.Grid(
+                    id="sensor-select-grid",
+                    justify="center",
+                    children=[
+                        dmc.GridCol(
+                            dmc.MultiSelect(
+                                id="sensors-multi-select",
+                                label="Sensoren",
+                                data=[
+                                    {
+                                        "value": "3453489234",
+                                        "label": "Temperatur-Sensor",
+                                    },
+                                    {
+                                        "value": "e454542343",
+                                        "label": "Luftdruck-Sensor",
+                                    },
+                                    {
+                                        "value": "3445652342",
+                                        "label": "Luftfeuchtigkeits-Sensor",
+                                    },
+                                    {"value": "2342342356", "label": "Wind-Sensor"},
+                                ],
+                            ),
+                            span={"base": 4, "xs": 12, "sm": 6, "md": 4},
+                        ),
+                        dmc.GridCol(
+                            dmc.DatePickerInput(
+                                id="date-input-range-picker",
+                                label="Zeitspanne",
+                                type="range",
+                                minDate=date(2020, 1, 1),
+                            ),
+                            span={"base": 4, "xs": 12, "sm": 6, "md": 4},
+                        ),
+                    ],
+                ),
                 dmc.Text(
                     id="output-status",
                     children="Dashboard loaded. Click button to fetch initial data.",
