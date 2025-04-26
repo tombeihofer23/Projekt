f0a5b7c3675339ab02b8aa8aea011c9d3173e470
Tom Beihofer <tom@sirius23.de>
2025-04-26 11:21:27 +0200

    Exceptions nicht in eigener Datei

--- src/hanoi_dashboard/utils/__init__.py
+++ src/hanoi_dashboard/utils/__init__.py
@@ -1,6 +1,5 @@
 """Modul für Util-Funktionen/Klassen."""
 
-from src.hanoi_dashboard.utils.exceptions import SensorDataPointExistsError
 from src.hanoi_dashboard.utils.sensor_data_validation_model import SensorDataModel
 
-__all__: list[str] = ["SensorDataPointExistsError", "SensorDataModel"]
+__all__: list[str] = ["SensorDataModel"]
--- src/hanoi_dashboard/utils/exceptions.py
+++
@@ -1,9 +0,0 @@
-from src.hanoi_dashboard.data import SensorData
-
-
-class SensorDataPointExistsError(Exception):
-    """Exception, falls ein Sensor-Datenpunkt bereits existiert."""
-
-    def __init__(self, sensor_data_point: SensorData) -> None:
-        super().__init__(f"Existierender Sensor-Datenpunkt: {sensor_data_point}")
-        self.sensor_data_point = sensor_data_point
