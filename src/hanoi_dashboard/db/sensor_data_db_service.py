c736936d23cef534926943be64a2882e429e1943
Tom Beihofer <tom@sirius23.de>
2025-04-26 20:30:10 +0200

    Methode, um viele Daten auf einmal in DB zu laden

--- src/hanoi_dashboard/db/sensor_data_db_service.py
+++ src/hanoi_dashboard/db/sensor_data_db_service.py
@@ -1,9 +1,14 @@
+import os
+from datetime import date
+from pathlib import Path
+from timeit import default_timer as timer
 from typing import Final
 
 import pandas as pd
 from loguru import logger
+from pandantic import Pandantic
 from pydantic import ValidationError
-from sqlalchemy import func, select
+from sqlalchemy import Date, cast, func, select
 from sqlalchemy.exc import SQLAlchemyError
 from sqlalchemy.orm import Session
 
@@ -55,6 +60,40 @@ class SensorDataWriteService:
         logger.info("Processed {} rows for box_id {}", insert_count, box_id)
         return insert_count
 
+    def bulk_write_sensor_data_to_db(self, data_dir_path: Path) -> None:
+        df_validator = Pandantic(schema=SensorDataModel)
+        paths: list = [name for name in os.listdir(data_dir_path)]
+        logger.debug(
+            "Start writing all historical data from '{}' in db.", data_dir_path
+        )
+        start = timer()
+        for path in paths:
+            sensor_id: str = path.split("=")[1]
+            logger.debug("Try to write data for sensor '{}' in db.", sensor_id)
+            df: pd.DataFrame = pd.read_parquet(data_dir_path.absolute() / path)
+            df["measurement"] = pd.to_numeric(df["measurement"], errors="coerce")
+
+            # Validate DataFrame with Pandantic
+            validated_df: pd.DataFrame = df_validator.validate(df, errors="skip")
+
+            with self.db_con.get_session()() as session:
+                validated_df.to_sql(
+                    name=SensorData.__tablename__,
+                    con=session.bind,
+                    if_exists="append",
+                    index=False,
+                    chunksize=5000,
+                    method="multi",
+                )
+            logger.debug("Wrote data for sensor '{}' in db.", sensor_id)
+
+        end = timer()
+        logger.debug(
+            "End writing all historical data from '{}' in db. Took {}s.",
+            data_dir_path,
+            end - start,
+        )
+
 
 class SensorDataQueryService:
     def __init__(self, db_con: DbCon) -> None:
@@ -64,6 +103,28 @@ class SensorDataQueryService:
         logger.info("Querying all data for box_id {}.", box_id)
         try:
             query = select(SensorData).where(SensorData.box_id == box_id)
+            with self.db_con.get_session()() as session:
+                df = pd.read_sql(query, session.bind, parse_dates=["timestamp"])
+            logger.info("Retrieved {} data points from db.", len(df))
+            return df
+        except SQLAlchemyError as e:
+            logger.error("SQLAlchemy error while querying data: {}", e)
+        except ValueError as e:
+            logger.error("ValueError while parsing result: {}", e)
+        except Exception as e:  # pylint: disable=broad-except
+            logger.exception("Unexpected error while querying data: {}", e)
+        return None
+
+    def query_data_from_a_date_on(self, box_id: str, from_date: date) -> pd.DataFrame:
+        logger.info(
+            "Querying data for box_id {} from {} to now.",
+            box_id,
+            from_date.strftime("%Y-%m-%d"),
+        )
+        try:
+            query = select(SensorData).where(
+                cast(SensorData.timestamp, Date) > from_date
+            )
             with self.db_con.get_session()() as session:
                 df = pd.read_sql(query, session.bind, parse_dates=["timestamp"])
             logger.info("Retrieved {} data points from db.", len(df))
@@ -103,11 +164,21 @@ class SensorDataDbService:
         self.sensor_data_write_service = SensorDataWriteService(self.db_con)
         self.sensor_data_query_servive = SensorDataQueryService(self.db_con)
 
+    # Write data in db
     def write_new_sensor_data(self, data: pd.DataFrame, box_id: str) -> int:
         return self.sensor_data_write_service.write_new_sensor_data(data, box_id)
 
+    def bulk_write_sensor_data_to_db(self, data_dir_path: Path) -> None:
+        self.sensor_data_write_service.bulk_write_sensor_data_to_db(data_dir_path)
+
+    # Get data from db
     def query_all_data(self, box_id: str) -> pd.DataFrame:
         return self.sensor_data_query_servive.query_all_data(box_id)
+
+    def query_data_from_a_date_on(self, box_id: str, from_date: date) -> pd.DataFrame:
+        return self.sensor_data_query_servive.query_data_from_a_date_on(
+            box_id, from_date
+        )
 
     # def query_data_date_range(
     #     self, box_id: str, start_date: str, end_date: str
@@ -119,5 +190,13 @@ class SensorDataDbService:
 
 if __name__ == "__main__":
     db_service = SensorDataDbService(DbCon())
-    qdata = db_service.query_all_data("6252afcfd7e732001bb6b9f7")
-    print(qdata)
+
+    tdf = db_service.query_data_from_a_date_on(
+        "5d6d5269953683001ae46adc", date(2025, 4, 15)
+    )
+    print(tdf.head())
+    print(len(tdf))
+    # data_path = (
+    #     Path(__file__).parent.parent.parent.parent / "data/5d6d5269953683001ae46adc"
+    # )
+    # db_service.bulk_write_sensor_data_to_db(data_path)
