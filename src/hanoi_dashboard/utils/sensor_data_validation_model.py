420e1e8adfc85233a3fa0de1cd39cc48f9b7f2b6
Tom Beihofer <tom@sirius23.de>
2025-04-26 11:23:14 +0200

    Datei aufgeräumt

--- src/hanoi_dashboard/utils/sensor_data_validation_model.py
+++ src/hanoi_dashboard/utils/sensor_data_validation_model.py
@@ -1,6 +1,6 @@
 """Pydantic-Model für die Sensor-Daten."""
 
-from pydantic import BaseModel, ValidationError
+from pydantic import BaseModel
 from pydantic.types import PastDatetime
 
 
@@ -16,20 +16,3 @@ class SensorDataModel(BaseModel):
     sensor_type: str | None = None
     icon: str | None = None
     title: str | None = None
-
-
-if __name__ == "__main__":
-    from src.hanoi_dashboard.components import SenseBoxApi
-
-    sense_box_api = SenseBoxApi("6252afcfd7e732001bb6b9f7")
-    sdf = sense_box_api.fetch_new_sensor_data()[:10]
-    sdf.at[1, "measurement"] = None
-
-    for _, row in sdf.iterrows():
-        try:
-            SensorDataModel.model_validate_json(row.to_json())
-            print(row.to_json())
-        except ValidationError as e:
-            print(e)
-        else:
-            print("test")
