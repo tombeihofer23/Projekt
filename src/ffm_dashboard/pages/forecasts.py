e8493a1286a2a70c3717a98c7da1e51b70643792
Tom Beihofer <tom@sirius23.de>
2025-06-28 01:52:39 +0200

    Forecast-Page angepasst

--- src/ffm_dashboard/pages/forecasts.py
+++ src/ffm_dashboard/pages/forecasts.py
@@ -1,6 +1,8 @@
 import dash
 import dash_mantine_components as dmc
 
+from src.ffm_dashboard.utils import get_infobox
+
 dash.register_page(__name__, path="/forecasts")
 
 layout = dmc.Box(
@@ -8,17 +10,46 @@ layout = dmc.Box(
         dmc.Box(
             m=15,
             children=[
-                dmc.Title(
-                    "Temperaturvorhersagen für die nächsten 1.5 Stunden", order=2
-                ),
-                dmc.Button(
-                    "New Forecast",
-                    id="fetch-forecast-button",
-                    variant="filled",
-                    color="red",
+                dmc.Flex(
+                    justify="space-between",
+                    mb=20,
+                    children=[
+                        dmc.Title(
+                            "Temperaturentwicklung", order=2, id="forecast-title"
+                        ),
+                        dmc.Button(
+                            "New Forecast",
+                            id="fetch-forecast-button",
+                            variant="filled",
+                            color="red",
+                        ),
+                    ],
                 ),
                 dmc.Box(
-                    children=[dmc.Box(id="forecast-graph-container", children=[])],
+                    mb=20,
+                    children=[dmc.Box(id="forecast-graph-container")],
+                ),
+                dmc.Card(
+                    shadow="sm",
+                    padding="lg",
+                    radius="md",
+                    withBorder=True,
+                    children=[
+                        dmc.Title("Modellinformationen", order=4),
+                        dmc.Group(
+                            # align="space-around",
+                            grow=True,
+                            children=[
+                                get_infobox("Modell", "MultiOutputLGBM"),
+                                get_infobox("Trainingsdaten", "364.433 Samples"),
+                                get_infobox("Testdaten", "91.109 Samples"),
+                                get_infobox("Features", "10"),
+                                get_infobox("MAE", "0.184 °C"),
+                            ],
+                        ),
+                        dmc.Divider(mb=15),
+                        dmc.Text("Letztes Training: 2025-06-27 14:22", size="sm"),
+                    ],
                 ),
             ],
         ),
