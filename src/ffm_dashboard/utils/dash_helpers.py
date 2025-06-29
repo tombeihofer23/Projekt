3feb44b464a2edd2d8a5019466ee0f5087ac3b4e
Tom Beihofer <tom@sirius23.de>
2025-06-29 18:04:05 +0200

    Kommentare für Utils

--- src/ffm_dashboard/utils/dash_helpers.py
+++ src/ffm_dashboard/utils/dash_helpers.py
@@ -1,8 +1,23 @@
+"""Hilfsfunktionen für Plotly Dash Elemente."""
+
 import dash_mantine_components as dmc
 from dash_iconify import DashIconify
 
 
 def get_infobox(header: str, info: str):
+    """
+    Erstellt eine optisch hervorgehobene Infobox im Stil einer Karte.
+
+    Die Box besteht aus einem Titel (fett) und einem kurzen Informationstext.
+
+    :param header: Überschrift der Infobox (z.B. "Letztes Update")
+    :type header: str
+    :param info: Inhalt bzw. Informationstext (z.B. "15 Minuten alt")
+    :type info: str
+    :return: Eine gestylte dmc.Paper-Komponente zur Anzeige der Info
+    :rtype: dash_mantine_components.Paper
+    """
+
     return dmc.Paper(
         withBorder=True,
         shadow="sm",
@@ -23,4 +38,15 @@ def get_infobox(header: str, info: str):
 
 
 def get_icon(icon: str, height: int) -> DashIconify:
+    """
+    Erstellt ein DashIconify-Icon.
+
+    :param icon: Icon-Identifier im Format "iconset:icon-name", z.B. "bi:cpu-fill"
+    :type icon: str
+    :param height: Höhe des Icons in Pixeln
+    :type height: int
+    :return: DashIconify-Komponente mit gewünschtem Icon und Größe
+    :rtype: DashIconify
+    """
+
     return DashIconify(icon=icon, height=height)
